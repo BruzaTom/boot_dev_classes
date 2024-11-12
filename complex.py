ea8ad1c05927b3df71fb6fb62a5e04771dcb422c
BruzaTom <thomasagomez77@gmail.com>
2024-11-11 22:34:00 -0600

    Order Log N

--- complex.py
+++ complex.py
@@ -316,3 +316,18 @@ def find_last_name2(names_dict, first_name):
         return names_dict[first_name]
     except KeyError:
         return None
+
+#binary search
+# note that the arrays values must be sorted     
+def binary_search(target, arr):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        med = (low + high) // 2
+        if arr[med] == target:
+            return True
+        elif arr[med] < target:
+            low = med + 1
+        else:
+            high = med - 1
+    return False
--- py_enviornment/main.py
+++ py_enviornment/main.py
@@ -1,9 +1,12 @@
-def find_last_name(names_dict, first_name):
-    if first_name in names_dict:
-        return names_dict[first_name]
-
-def find_last_name2(names_dict, first_name):
-    try:
-        return names_dict[first_name]
-    except KeyError:
-        return None
+def binary_search(target, arr):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        med = (low + high) // 2
+        if arr[med] == target:
+            return True
+        elif arr[med] < target:
+            low = med + 1
+        else:
+            high = med - 1
+    return False
