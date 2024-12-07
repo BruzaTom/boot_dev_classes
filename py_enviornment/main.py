3bb04d072cbf2505216cc3cc127acbb1163b9b69
BruzaTom <thomasagomez77@gmail.com>
2024-12-06 20:57:29 -0600

    Merge Sort

--- py_enviornment/main.py
+++ py_enviornment/main.py
@@ -1,14 +1,26 @@
-def bubble_sort(nums):
-    swapping = True
-    end = len(nums)
-    while swapping == True:
-        swapping = False
-        for i in range(1, end):
-            if nums[i-1] > nums[i]:
-                num = nums[i-1]
-                nums[i-1] = nums[i]
-                nums[i] = num
-                swapping = True
-        end -= 1
-    return nums
+def merge_sort(nums):
+    if len(nums) < 2:
+        return nums
+    mid = len(nums) // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+    #print(lst1,'\n',lst2)
+    return merge(left, right)
+
+
+def merge(first, second):
+    final = []
+    i, j = 0, 0
+    #print(first,second)
+    while((i < len(first)) & (j < len(second))):
+            if first[i] <= second[j]:
+                final.append(first[i])
+                i += 1
+            else:
+                final.append(second[j])
+                j += 1
+    final.extend(first[i:])
+    final.extend(second[j:])
+    return final
+
 
