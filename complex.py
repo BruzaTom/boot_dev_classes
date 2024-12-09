53a9a620b917f86072d36445c2c878adef3099a8
BruzaTom <thomasagomez77@gmail.com>
2024-12-08 18:21:59 -0600

    insertion sort

--- complex.py
+++ complex.py
@@ -371,3 +371,14 @@ def merge(first, second):
     final.extend(first[i:])
     final.extend(second[j:])
     return final
+
+#insertion sort
+def insertion_sort(nums):
+    for i in range(0, len(nums)):
+        j = i
+        while((j > 0) & (nums[j-1] > nums[j])):
+            temp = nums[j]
+            nums[j] = nums[j-1]
+            nums[j-1] = temp
+            j -= 1
+    return nums
--- py_enviornment/main.py
+++ py_enviornment/main.py
@@ -1,26 +1,11 @@
-def merge_sort(nums):
-    if len(nums) < 2:
-        return nums
-    mid = len(nums) // 2
-    left = merge_sort(nums[:mid])
-    right = merge_sort(nums[mid:])
-    #print(lst1,'\n',lst2)
-    return merge(left, right)
-
-
-def merge(first, second):
-    final = []
-    i, j = 0, 0
-    #print(first,second)
-    while((i < len(first)) & (j < len(second))):
-            if first[i] <= second[j]:
-                final.append(first[i])
-                i += 1
-            else:
-                final.append(second[j])
-                j += 1
-    final.extend(first[i:])
-    final.extend(second[j:])
-    return final
+def insertion_sort(nums):
+    for i in range(0, len(nums)):
+        j = i
+        while((j > 0) & (nums[j-1] > nums[j])):
+            temp = nums[j]
+            nums[j] = nums[j-1]
+            nums[j-1] = temp
+            j -= 1
+    return nums
 
 
