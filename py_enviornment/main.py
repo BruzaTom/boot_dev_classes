29d20874ad55ba310c8d90de21ac8969a6e5efea
BruzaTom <thomasagomez77@gmail.com>
2025-05-03 17:10:10 -0500

    Matchmaking Queue

--- py_enviornment/main.py
+++ py_enviornment/main.py
@@ -1,45 +1,15 @@
-class Queue:# my answer
-    def __init__(self):
-        self.items = []
+from queue import Queue
 
-    def push(self, item):
-        self.items.insert(0, item)
 
-    def pop(self):
-        if len(self.items) > 0:
-            return self.items.pop()
-        else:
-            return None
-
-    def peek(self):
-        if len(self.items) > 0:
-            return self.items[-1]
-        else:
-            return None
-
-    def size(self):
-        return len(self.items)
-    
-class solution_Queue:# solution answer
-    def __init__(self):
-        self.items = []
-
-    def push(self, item):
-        self.items.insert(0, item)
-
-    def pop(self):
-        if len(self.items) == 0:
-            return None
-        temp = self.items[-1]
-        del self.items[-1]
-        return temp
-
-    def peek(self):
-        if len(self.items) == 0:
-            return None
-        return self.items[-1]
-
-    def size(self):
-        return len(self.items)
-
-    
+def matchmake(queue, user):
+    if user[1] == 'join':
+        queue.push(user[0])
+    if user[1] == 'leave':
+        if user[0] in queue.items:
+            queue.search_and_remove(user[0])
+    if queue.size() >= 4:
+         user1 = queue.pop()
+         user2 = queue.pop()
+         return f"{user1} matched {user2}!"
+    else:
+        return 'No match found'
