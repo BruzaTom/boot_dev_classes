b28cf11300e743dc98a0b29c56fde92adf544043
tomux <thomasagomez77@gmail.com>
2025-08-14 02:16:33 -0500

    add_to_tail_f

--- py_enviornment/main.py
+++ py_enviornment/main.py
@@ -2,6 +2,20 @@ from node import Node
 
 
 class LinkedList:
+    def add_to_tail(self, node):
+        # If the list is empty, set head to the new node
+        if self.head == None:
+            self.head = node
+        else:
+            # Otherwise, traverse to the last node
+            last_node = self.head
+            while last_node.next:
+                last_node = last_node.next
+            # Set the last node's next to the new node
+            last_node.next = node
+
+    # don't touch below this line
+
     def __init__(self):
         self.head = None
 
@@ -11,12 +25,9 @@ class LinkedList:
             yield node
             node = node.next
 
-    # don't touch below this line
-
     def __repr__(self):
         nodes = []
-        current = self.head
-        while current and hasattr(current, "val"):
-            nodes.append(current.val)
-            current = current.next
+        for node in self:
+            nodes.append(node.val)
         return " -> ".join(nodes)
+
