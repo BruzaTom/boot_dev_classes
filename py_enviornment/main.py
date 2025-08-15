195e92c6ae6fe79ccdf481313025af3a573c5e3c
tomux <thomasagomez77@gmail.com>
2025-08-15 00:14:28 -0500

    remove_from_head_f

--- py_enviornment/main.py
+++ py_enviornment/main.py
@@ -1,28 +1,30 @@
 from node import Node
 
 
-class LinkedList:
-    def add_to_head(self, node):
+class LLQueue:
+    def remove_from_head(self):
         if self.head == None:
-            self.tail = node
-            self.head = node
-            return
-        node.set_next(self.head)
-        self.head = node
+            return None
+        popped = self.head
+        self.head = self.head.next
+        if self.head == None:
+            self.tail = None
+        popped.set_next(None)
+        return popped
+
+    # don't touch below this line
 
     def add_to_tail(self, node):
         if self.head is None:
-            self.tail = node
             self.head = node
+            self.tail = node
             return
         self.tail.set_next(node)
         self.tail = node
 
     def __init__(self):
-        self.head = None
         self.tail = None
-
-    # don't touch below this line
+        self.head = None
 
     def __iter__(self):
         node = self.head
@@ -34,5 +36,5 @@ class LinkedList:
         nodes = []
         for node in self:
             nodes.append(node.val)
-        return " -> ".join(nodes)
+        return " <- ".join(nodes)
 
