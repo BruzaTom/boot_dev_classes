3606e2f2820d6270345ff5ee346de686a0dc3b51
tomux <thomasagomez77@gmail.com>
2025-08-14 20:49:29 -0500

    add_to_head_f

--- py_enviornment/main.py
+++ py_enviornment/main.py
@@ -1,20 +1,23 @@
 from node import Node
 
-
 class LinkedList:
-    def add_to_tail(self, node):
-        # If the list is empty, set head to the new node
-        if self.head == None:
-            self.head = node
-        else:
-            # Otherwise, traverse to the last node
-            last_node = self.head
-            while last_node.next:
-                last_node = last_node.next
-            # Set the last node's next to the new node
-            last_node.next = node
+    def add_to_head(self, node):
+        #trying to use node.set_next(node)
+        #insted of just using node.next = node
+        #directly
+        node.set_next(self.head)
+        self.head = node
 
     # don't touch below this line
+
+    def add_to_tail(self, node):
+        if self.head is None:
+            self.head = node
+            return
+        last_node = None
+        for current_node in self:
+            last_node = current_node
+        last_node.set_next(node)
 
     def __init__(self):
         self.head = None
