369f8fe98235871e8dfe7b50c90f53ae90325864
tomux <thomasagomez77@gmail.com>
2025-11-16 23:22:10 -0600

    stopped dsa and started docker lessons ch1 l1-2

--- py_enviornment/main.py
+++ py_enviornment/main.py
@@ -1,39 +1,20 @@
-class BSTNode:
-    def height(self):
-        left, right = 0, 0
-        if self.val == None:
-            return 0
-        if self.left:
-            left = self.left.height()
-        if self.right:
-            right = self.right.height()
-        return max(left, right) + 1
-
-
-    # don't touch below this line
-
-    def __init__(self, val=None):
+class RBNode:
+    def __init__(self, val):
+        self.red = False
+        self.parent = None
+        self.val = val
         self.left = None
         self.right = None
-        self.val = val
+
+
+class RBTree:
+    def __init__(self):
+        self.nil = RBNode(None)
+        self.nil.red = False
+        self.nil.left = None
+        self.nil.right = None
+        self.root = self.nil
 
     def insert(self, val):
-        if not self.val:
-            self.val = val
-            return
-
-        if self.val == val:
-            return
-
-        if val < self.val:
-            if self.left:
-                self.left.insert(val)
-                return
-            self.left = BSTNode(val)
-            return
-
-        if self.right:
-            self.right.insert(val)
-            return
-        self.right = BSTNode(val)
+        pass
 
